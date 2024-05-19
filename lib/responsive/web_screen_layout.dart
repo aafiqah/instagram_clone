0f65b1b459fa0e57881119dbe372d77f5e21fec4
Alessa Afiqah <alessajohar@gmail.com>
2024-05-19 23:51:03 +0800

    build responsive layout widget

---
+++ lib/responsive/web_screen_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WebsiteScreenLayout extends StatelessWidget {
+  const WebsiteScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('web')),);
+  }
+}
