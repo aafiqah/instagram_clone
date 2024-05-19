f68bc23c8d176acd768622590db35e60ff3e06e1
Alessa Afiqah <alessajohar@gmail.com>
2024-05-20 00:30:33 +0800

    add firebase for android and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCJLaNGr4eTZUnXQK2CWGKhrniD9uZ1tGw',
+        appId: '1:636073620275:web:b9db016f528402f74e0446',
+        messagingSenderId: '636073620275',
+        projectId: 'instagram-clone-55427',
+        storageBucket: 'instagram-clone-55427.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
